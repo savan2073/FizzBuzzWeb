8139181a59fa325f3fb6a6f3ebfefcbf375dddbe
Bartosz Sawaniewski <savan2073@gmail.com>
2023-03-15 14:54:37 +0100

    zadanie 3 sesjafinal

---
+++ Models/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Range(1899, 2022, ErrorMessage = "Oczekiwana wartość {0} z zakresu <{1} i {2}>.")]
+        [Required(ErrorMessage = "Pole Rok Urodzenia nie może być puste")]
+        [Display(Name = "Rok urodzenia")]
+        public int BirthYear { get; set; }
+
+        [RegularExpression("[A-Za-z]*", ErrorMessage = "Nie możesz używać liczb oraz znaków specjalnych")]
+        [Required(ErrorMessage = "Pole Imię nie może być puste")]
+        [Display(Name = "Imię")]
+        [MaxLength(100)]
+
+        public string FirstName { get; set; }
+
+        public bool LeapYear { get; set; }
+
+        public void CheckLeapYear()
+        {
+            LeapYear = ((BirthYear % 4 == 0) && (BirthYear % 100 != 0)) || (BirthYear % 400 == 0);
+        }
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FizzBuzzWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        [BindProperty]
+        public FizzBuzz FizzBuzz { set; get; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public string AlertMessage { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +21,53 @@ namespace FizzBuzzWeb.Pages
 
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
 
+        }
+
+        public IActionResult OnPost()
+        {
+            var dataList = new List<FizzBuzz>();
+
+            var sessionData = HttpContext.Session.GetString("Data");
+            if (!string.IsNullOrEmpty(sessionData))
+            {
+                dataList = JsonConvert.DeserializeObject<List<FizzBuzz>>(sessionData);
+            }
+
+
+
+            if (FizzBuzz.BirthYear == 0)
+            {
+                ModelState.Remove("FizzBuzz.BirthYear");
+                ModelState.AddModelError("FizzBuzz.BirthYear", "Pole Rok urodzenia nie może być puste");
+            }
+            if (ModelState.IsValid)
+            {
+                AlertMessage = "Podane wartości są nieprawidłowe";
+                return Page();
+            }
+            if(FizzBuzz.BirthYear >= 1899 && FizzBuzz.BirthYear <=2022)
+            {
+                dataList.Add(FizzBuzz);
+            }
+
+            FizzBuzz.CheckLeapYear();
+            HttpContext.Session.SetString("Data", JsonConvert.SerializeObject(dataList));
+
+            if (FizzBuzz.LeapYear)
+            {
+                AlertMessage = $"{FizzBuzz.FirstName} urodził/a się w {FizzBuzz.BirthYear} roku. To był rok przestępny";
+            }
+            else
+            {
+                AlertMessage = $"{FizzBuzz.FirstName} urodził/a się w {FizzBuzz.BirthYear} roku. To nie był rok przestępny";
+            }
+
+            return Page();
         }
     }
 }
